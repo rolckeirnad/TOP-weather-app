c320dfa30e91935171c1dc6eec084dd1512b43ec
Daniel Gallardo <dgallardo.c@outlook.com>
2022-08-14 14:51:05 -0500

    Change output assets directory

--- webpack.common.js
+++ webpack.common.js
@@ -18,6 +18,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          outputPath: 'assets/',
+        },
       },
     ],
   },
